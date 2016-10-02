8d1a64b6b437554d336a4e9edfcc92778b0c7f6b
alx <otinanism@gmai.com>
2016-10-02 22:12:07 +0300

    Initial commit

---
+++ src/main/java/gr/alx/ReleaseManagementApplication.java
@@ -0,0 +1,12 @@
+package gr.alx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleaseManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReleaseManagementApplication.class, args);
+	}
+}
