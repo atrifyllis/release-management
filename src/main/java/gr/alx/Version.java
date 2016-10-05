85db40ac186aa5e25e3814b494a495a5c5e51c95
alx <otinanism@gmai.com>
2016-10-05 21:56:48 +0300

    fix(infrastructure): use lombok in model class

--- src/main/java/gr/alx/Version.java
+++ src/main/java/gr/alx/Version.java
@@ -1,53 +1,21 @@
 package gr.alx;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Created by alx on 10/2/2016.
  */
+@Getter
+@Setter
+@AllArgsConstructor
 public class Version {
 
     Integer major;
     Integer minor;
     Integer build;
     boolean isSnapshot;
-
-    public Version(Integer major, Integer minor, Integer build, boolean isSnapshot) {
-        this.major = major;
-        this.minor = minor;
-        this.build = build;
-        this.isSnapshot = isSnapshot;
-    }
-
-    public Integer getMajor() {
-        return major;
-    }
-
-    public void setMajor(Integer major) {
-        this.major = major;
-    }
-
-    public Integer getMinor() {
-        return minor;
-    }
-
-    public void setMinor(Integer minor) {
-        this.minor = minor;
-    }
-
-    public Integer getBuild() {
-        return build;
-    }
-
-    public void setBuild(Integer build) {
-        this.build = build;
-    }
-
-    public boolean isSnapshot() {
-        return isSnapshot;
-    }
-
-    public void setSnapshot(boolean snapshot) {
-        isSnapshot = snapshot;
-    }
 
     @Override
     public String toString() {
